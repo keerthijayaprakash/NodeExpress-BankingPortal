4b986f86144c0188ef0ad8a7ee2e1c1ef8f137f4
Keerthi Jayaprakash <zjayaprk01@mscdirect.com>
2020-12-19 21:53:23 +0530

    Completed module# 3

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 const express = require('express');
+const { urlencoded } = require('express');
 const app = express();
 
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, './public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(path.join(__dirname, './json/accounts.json'), { encoding: 'utf8' });
 const accounts = JSON.parse(accountData);
@@ -25,6 +27,37 @@ app.get('/checking', function (request, response) {
 
 app.get('/credit', function (request, response) {
     response.render('account', { account: accounts.credit });
+});
+
+app.get('/transfer', function (request, response) {
+    response.render('transfer');
+});
+
+app.post('/transfer', function (request, response) {
+    var fromAccount = request.body.from;
+    var toAccount = request.body.to;
+    var transferAmount = parseInt(request.body.amount);
+    accounts[fromAccount].balance -= transferAmount;
+    accounts[toAccount].balance += transferAmount;
+
+    var accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, './json/accounts.json'), accountsJSON, { encoding: 'utf8' });
+
+    response.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', function (request, response) {
+    response.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', function (request, response) {
+    accounts.credit.balance -= parseInt(request.body.amount);
+    accounts.credit.available += parseInt(request.body.amount)
+
+    var accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, './json/accounts.json'), accountsJSON, { encoding: 'utf8' });
+
+    response.render('payment', { message: 'Payment Successful', account: accounts.credit });
 });
 
 const userData = fs.readFileSync(path.join(__dirname, './json/users.json'), { encoding: 'utf8' });
